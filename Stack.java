4bef60cc4624a0747e3613fce4b2a4b2f2c77eaa
Michael Mebane <michael.mebane@gmail.com>
2017-07-28 11:08:44 -0400

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,8 @@
+//Write your code here
+
+Set<String> pairs = new HashSet<>();
+
+for(int i = 0; i<t; i++){
+    pairs.add(pair_left[i]+" "+pair_right[i]);    
+    System.out.println(pairs.size());
+}
