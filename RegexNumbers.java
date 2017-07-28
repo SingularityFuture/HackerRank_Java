f7798cc0e00486970b9254ed082a1d81d9852ea5
Michael Mebane <michael.mebane@gmail.com>
2017-07-28 11:16:59 -0400

    Create RegexNumbers.java

---
+++ RegexNumbers.java
@@ -0,0 +1,10 @@
+//Write your code here
+class MyRegex{
+    
+    String pattern;
+    
+    MyRegex(){
+        String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+        this.pattern = zeroTo255 + "." + zeroTo255 + "." + zeroTo255 + "." + zeroTo255;
+    }
+}
